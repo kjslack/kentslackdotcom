0d8acd4c8d2a7d7119537f71c0cba233a346c4c1
Unknown <kent.slack@gmail.com>
2018-05-23 22:28:44 -0700

    Complete re-write of the python script.
    Now takes in the url and times and creates the embed code.

--- familyhistory/time_in_seconds.py
+++ familyhistory/time_in_seconds.py
@@ -1,18 +1,22 @@
 import pyperclip
 
-while True:
+
+def GetTime(text_header):
+    print(text_header)
     hours = raw_input("Hours: ")
-    if( hours == -1):
-        exit()
     minutes = raw_input("Minutes: ")
     seconds = raw_input("Seconds: ")
-
     total_seconds = (int(hours) * 3600) + (int(minutes) * 60) + int(seconds)
     print(total_seconds)
-    pyperclip.copy(str(total_seconds))
+    return total_seconds
 
-#def GetTime():
-#    hours = raw_input("Hours: ")
-#    minutes = raw_input("Minutes: ")
-#    seconds = raw_input("Seconds: ")
-#    total_seconds = (int(hours) * 3600) + (int(minutes) * 60) + int(seconds)
+url = raw_input("Youtube Video URL = ")
+
+while True:    
+    start_time = GetTime("Start Time");
+    end_time = GetTime("End Time");
+    
+    html_embed_code = '<iframe width="300" height="169" src="' + url + '?rel=0&amp;start=' + str(start_time) + ';end=' + str(end_time) + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+    pyperclip.copy(html_embed_code)
+    print(html_embed_code)
+    print('\n\n')
